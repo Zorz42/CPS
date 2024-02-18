098b54d45d6c006de6cfb5db7c0efffd5bdd6f6c
Jakob Zorz <54270248+Zorz42@users.noreply.github.com>
2024-02-18 12:41:33 +0100

    this is a ugly way to get a file but it will do ;)

--- src/submission.rs
+++ src/submission.rs
@@ -42,25 +42,58 @@ impl SubmissionDatabase {
     }
 }
 
+async fn extract_file_from_request(request: Request<Incoming>) -> Result<String> {
+    let boundary = request
+        .headers()
+        .get("content-type")
+        .and_then(|ct| ct.to_str().ok())
+        .and_then(|ct| {
+            let parts: Vec<&str> = ct.split(';').collect();
+            let mut boundary = None;
+
+            for part in parts {
+                let parts: Vec<&str> = part.trim().split('=').collect();
+                if parts.len() != 2 {
+                    continue;
+                }
+
+                if parts[0] == "boundary" {
+                    boundary = Some(parts[1].to_string());
+                }
+            }
+
+            boundary
+        })
+        .unwrap_or("no-boundary".to_string());
+
+    let boundary = format!("--{}", boundary);
+
+    let body = request.into_body().collect().await?.to_bytes();
+    let body = String::from_utf8_lossy(&body).to_string();
+
+    let mut parts = body.split(&boundary).collect::<Vec<&str>>();
+    parts.retain(|x| !x.is_empty());
+    parts.pop();
+    let part = parts[0];
+    let mut code_parts = part.split("\r\n").collect::<Vec<&str>>();
+    code_parts.remove(0);
+    code_parts.remove(0);
+    code_parts.remove(0);
+    code_parts.remove(0);
+    code_parts.pop();
+
+    Ok(code_parts.join("\r\n\r\n"))
+}
+
 pub async fn handle_submission_form(
     state: &GlobalState,
     user_id: Option<UserId>,
     problem_id: &str,
     request: Request<Incoming>,
 ) -> Result<Option<Response<Full<Bytes>>>> {
-    let body = request.into_body().collect().await?.to_bytes();
-    let body = String::from_utf8_lossy(&body).to_string();
+    let code = extract_file_from_request(request).await?;
 
-    // erase first 3 lines and last line
-    let mut lines = body.lines();
-    lines.next();
-    lines.next();
-    lines.next();
-    let mut lines = lines.collect::<Vec<&str>>();
-    lines.pop();
-    let code = lines.join("\n");
-
-    println!("Received code: {}", code);
+    println!("Received code: {:?}", vec![code]);
 
     Ok(None)
 }
