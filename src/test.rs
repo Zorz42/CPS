799c68410b41e5af951fd3fcb0a0b3195e785f31
Jakob Zorz <54270248+Zorz42@users.noreply.github.com>
2024-02-19 14:10:23 +0100

    made a dysfunctional database manager

--- src/test.rs
+++ src/test.rs
@@ -1,27 +1,2 @@
-use crate::id::GenericId;
-use crate::submission::EvaluationStatus;
-use std::path::PathBuf;
-
-pub type TestId = GenericId;
-pub type SubtaskId = GenericId;
-
-pub struct EvaluationSubtask {
-    pub status: EvaluationStatus,
-    pub tests: Vec<EvaluationTest>,
-}
-
-pub struct EvaluationTest {
-    pub status: EvaluationStatus,
-}
-
-pub struct ProblemSubtask {
-    pub tests: Vec<ProblemTest>,
-    pub subtask_id: SubtaskId,
-}
-
-pub struct ProblemTest {
-    pub problem_id: u128,
-    pub test_id: TestId,
-    pub input_file: PathBuf,
-    pub output_file: PathBuf,
-}
+pub type TestId = i32;
+pub type SubtaskId = i32;
